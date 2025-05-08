f363096fc56cbb44e35873858c939bd7dc026393
Shams Emam <147142639+ShamsEmam@users.noreply.github.com>
2025-05-08 17:42:06 +0300

    Update Program.cs

--- CafeSystem/Program.cs
+++ CafeSystem/Program.cs
@@ -5,9 +5,7 @@ namespace CafeSystem
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
